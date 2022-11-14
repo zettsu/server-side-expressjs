56e48a7805a9bcc8e399b095ff6296af927c7da7
Matias Olivera <jmatias.olivera@gmail.com>
2022-11-14 01:45:40 -0300

    init project bikes

--- app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-app.get('/', function(req, res) {
-    res.send('Hola Mundo!');
-});
-app.listen(3000, function() {
-    console.log('Aplicación ejemplo, escuchando el puerto 3000!');
-}); 
