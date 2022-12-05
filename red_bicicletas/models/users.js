dac9ed2d43388c1979b386a0414b6ceb3c9dc5b5
Matias Olivera <jmatias.olivera@gmail.com>
2022-12-05 00:49:07 -0300

    added security to users model

--- red_bicicletas/models/users.js
+++ red_bicicletas/models/users.js
@@ -1,18 +1,57 @@
 let mongoose = require('mongoose');
+let bcrypt = require('bcrypt');
 let Booking = require('../models/booking');
 
 let Schema = mongoose.Schema;
 
+const saltRounds = 10;
+
+const validateEmail = function (email) {
+    return "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/".test(email);
+}
+
 let usersSchema = new Schema({
-    name: String,
+    name: {
+        type: String,
+        trim: true,
+        required: [true, "El email es obligatorio"]
+    },
+    email:{
+        type: String,
+        trim: true,
+        required: [true, "El email es obligatorio"],
+        lowercase: [validateEmail, "Por favor, ingrese un email valido"],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+    },
+    password: {
+        type: String,
+        required: [true, "El password es obligatorio"]
+    },
+    passwordResetToken: String,
+    passwordResetTokenExpires: Date,
+    verified: {
+        type: Boolean,
+        default: false
+    }
 });
 
-usersSchema.methods.createBooking = function(bikeId, from, to, cb){
+usersSchema.pre('save', function (next) {
+    if (this.isModified('password')) {
+        this.password = bcrypt.hashSync(this.password, saltRounds);
+    }
+    next();
+});
+
+usersSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+}
+
+usersSchema.methods.createBooking = function(bikeId, from, to, cb) {
     const booking = new Booking({user:this._id, bike:bikeId, from:from, to:to});
     booking.save(cb);
 }
 
-usersSchema.statics.findAll = function(cb){
+usersSchema.statics.findAll = function(cb) {
     return this.find({}, cb);
 }
 
@@ -20,7 +59,7 @@ usersSchema.statics.findByUserId = function (id, cb) {
     return this.findById(id, cb);
 }
 
-usersSchema.statics.addUser = function(name, cb){
+usersSchema.statics.addUser = function(name, cb) {
     return this.create({ name:name }, cb);
 }
 
