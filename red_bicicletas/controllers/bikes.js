423069beb73b769008b5e14b4143b8d7795bd97e
Matias Olivera <jmatias.olivera@gmail.com>
2022-12-05 01:27:26 -0300

    Added mailer

--- red_bicicletas/controllers/bikes.js
+++ red_bicicletas/controllers/bikes.js
@@ -1,6 +1,8 @@
 let Bikes = require("../models/bikes");
+const {main} = require('../mailer/Mailer');
 
 exports.json_bikes = function (req, res) {
+
     res.json({ bikes : Bikes.findAll() });
 }
 
@@ -9,6 +11,7 @@ exports.list_bikes = async function (req, res) {
     if (bikes === null){
         bikes = []
     }
+    main().catch(console.error);
     res.render('bikes/index', { bikes : bikes });
 }
 
